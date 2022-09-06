891ba87e5b4c1ac94f70e46134bb5a32c4c3ff87
Dhanushka Ranasinghe <36202354+DhanushkaRanasinghe@users.noreply.github.com>
2022-09-06 12:52:17 +0530

    Files

---
+++ index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>HMS</title>
+    <!-- meta tags -->
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="keywords" content="Art Sign Up Form Responsive Widget, Audio and Video players, Login Form Web Template, Flat Pricing Tables, Flat Drop-Downs, Sign-Up Web Templates,
+		Flat Web Templates, Login Sign-up Responsive Web Template, Smartphone Compatible Web Template, Free Web Designs for Nokia, Samsung, LG, Sony Ericsson, Motorola Web Design"
+    />
+    <!-- /meta tags -->
+    <!-- custom style sheet -->
+    <link href="web/css/style.css" rel="stylesheet" type="text/css" />
+    <!-- /custom style sheet -->
+    <!-- fontawesome css -->
+    <link href="web/css/fontawesome-all.css" rel="stylesheet" />
+    <!-- /fontawesome css -->
+    <!-- google fonts-->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat">>
+    <!-- /google fonts-->
+
+</head>
+
+
+<body>
+    <h1>Hostel Room Allocation System</h1>
+    <div class=" w3l-login-form">
+        <h2>Student Login</h2>
+        <form action="includes/login.inc.php" method="POST">
+
+            <div class=" w3l-form-group">
+                <label>Student Roll No:</label>
+                <div class="group">
+                    <i class="fas fa-user"></i>
+                    <input type="text" class="form-control" name="student_roll_no" placeholder="Roll No" required="required" />
+                </div>
+            </div>
+            <div class=" w3l-form-group">
+                <label>Password:</label>
+                <div class="group">
+                    <i class="fas fa-unlock"></i>
+                    <input type="password" class="form-control" name="pwd" placeholder="Password" required="required" />
+                </div>
+            </div>
+            <!--<div class="forgot">
+                <a href="#">Forgot Password?</a>
+                <p><input type="checkbox">Remember Me</p>
+            </div>-->
+            <button type="submit" name="login-submit">Login</button>
+        </form>
+          <p class=" w3l-register-p">Login as<a href="login-hostel_manager.php" class="register"> Hostel-Manager/Admin</a></p>
+          <p class=" w3l-register-p">Login as<a href="login-hostel_subwarden.php" class="register"> Hostel-Subwarden</a></p>
+        <p class=" w3l-register-p">Don't have an account?<a href="signup.php" class="register"> Sign up</a></p>
+    </div>
+    <footer>
+        <p >  UWU WELLESSA UNIVERSITY|@2022 </a></p>
+    </footer>
+
+</body>
+
+</html>
